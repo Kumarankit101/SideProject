86e3c2698e6473d657a254684c0279109046d405
Ankit Kumar <kumar.ankit.101012@gmail.com>
2025-03-30 16:54:18 +0530

    vercel fix

--- vite.config.ts
+++ vite.config.ts
@@ -30,8 +30,15 @@ export default defineConfig({
     },
   },
   root: path.resolve(__dirname, "client"),
+  publicDir: path.resolve(__dirname, "client/public"),
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
-    emptyOutDir: true
+    emptyOutDir: true,
+    assetsDir: "assets",
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "client/index.html"),
+      },
+    }
   }
 })
